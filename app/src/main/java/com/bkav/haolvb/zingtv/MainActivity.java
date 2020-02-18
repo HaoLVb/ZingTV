1045913badadcb14667cbf15806b8007dab9e4f4
HaoLVb <haovanle2210@gmail.com>
2020-02-18 18:29:23 +0700

    Initial commit

---
+++ app/src/main/java/com/bkav/haolvb/zingtv/MainActivity.java
@@ -0,0 +1,140 @@
+package com.bkav.haolvb.zingtv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
+import com.jarvanmo.exoplayerview.media.SimpleMediaSource;
+import com.jarvanmo.exoplayerview.ui.ExoVideoPlaybackControlView;
+import com.jarvanmo.exoplayerview.ui.ExoVideoView;
+
+import static com.jarvanmo.exoplayerview.orientation.OnOrientationChangedListener.SENSOR_LANDSCAPE;
+import static com.jarvanmo.exoplayerview.orientation.OnOrientationChangedListener.SENSOR_PORTRAIT;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoView = findViewById(R.id.videoView);
+
+        initControllerMode();
+        initVideoView();
+    }
+
+
+    private ExoVideoView videoView;
+
+    private void initControllerMode() {
+        //set resize_mode_fit
+        videoView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIT);
+
+        //set controller mode
+        int mode = 0;
+        mode |= ExoVideoPlaybackControlView.CONTROLLER_MODE_ALL;
+        videoView.setControllerDisplayMode(mode);
+    }
+
+    private void initVideoView() {
+        videoView.setPortrait(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT);
+        videoView.setBackListener((view, isPortrait) -> {
+            if (isPortrait) {
+                finish();
+            }
+            return false;
+        });
+
+        videoView.setOrientationListener(orientation -> {
+            if (orientation == SENSOR_PORTRAIT) {
+                changeToPortrait();
+            } else if (orientation == SENSOR_LANDSCAPE) {
+                changeToLandscape();
+            }
+        });
+
+        SimpleMediaSource mediaSource = new SimpleMediaSource("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+        mediaSource.setDisplayName("Apple HLS");
+        videoView.play(mediaSource, false);
+
+    }
+
+    private void changeToPortrait() {
+
+        // WindowManager operation is not necessary
+        WindowManager.LayoutParams attr = getWindow().getAttributes();
+//        attr.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Window window = getWindow();
+        window.setAttributes(attr);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+
+    private void changeToLandscape() {
+        // WindowManager operation is not necessary
+
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+//        lp.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        Window window = getWindow();
+        window.setAttributes(lp);
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (Build.VERSION.SDK_INT > 23) {
+            videoView.resume();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if ((Build.VERSION.SDK_INT <= 23)) {
+            videoView.resume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Build.VERSION.SDK_INT <= 23) {
+            videoView.pause();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Build.VERSION.SDK_INT > 23) {
+            videoView.pause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        videoView.releasePlayer();
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return videoView.onKeyDown(keyCode, event);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
